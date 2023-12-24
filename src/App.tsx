8b164f694b5494af06798841fc248464c4179287
Maël Briantin <mael.briantin@proton.me>
2023-12-24 23:39:11 +0100

    feat(toast): add toast component for error handling and notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Router } from "./routes/Router";
 import { AuthProvider } from "./contexts/auth/AuthContext";
+import { ToastProvider } from "./contexts/global/ToastContext";
 
 function App() {
 
   return (
     <AuthProvider>
-      <Router />
+      <ToastProvider>
+        <Router />
+      </ToastProvider>
     </AuthProvider>
   )
 }
--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import { Outlet } from "react-router-dom";
+import { Toast } from "../components/global/Toast";
 
 export const GuestLayout = () => {
     return (
         <GuestLayoutStyle >
             <Outlet />
+            <Toast />
         </GuestLayoutStyle>
     );
 };
 
 const GuestLayoutStyle = styled.div`
+    position: relative;
     height: 100vh;
     width: 100vw;
     overflow: hidden;
