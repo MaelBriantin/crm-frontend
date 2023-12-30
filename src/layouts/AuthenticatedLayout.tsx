3895c646cfab191c65c4d18a87ca66597cee862e
Maël Briantin <mael.briantin@proton.me>
2023-12-30 21:53:15 +0100

    feat(navbar): add initial implementation of navbar

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import { Toast } from "../components/global/Toast";
+import { Navbar } from "../components/global/Navbar";
+import { theme } from "../assets/themes";
+import { ShowNavbarButton } from "../components/global/ShowNavbarButton";
+import { useState } from "react";
 
 export const AuthenticatedLayout = () => {
+    const [showNavbar, setShowNavbar] = useState(true);
     return (
         <AuthenticatedLayoutStyle>
-            <Outlet />
+            <Navbar showNavbar={showNavbar} setShowNavbar={setShowNavbar} />
+            <OutletContainer>
+                <Outlet />
+                <ShowNavbarButton showNavbar={showNavbar} setShowNavbar={setShowNavbar} />
+            </OutletContainer>
             <Toast />
         </AuthenticatedLayoutStyle>
     );
@@ -19,4 +28,21 @@ const AuthenticatedLayoutStyle = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    background: whitesmoke;
 `;
+
+const OutletContainer = styled.div`
+    position: relative;
+    transition: all 250ms;
+    z-index: 99;
+    height: 98%;
+    width: 100%;
+    margin: 0 0.5%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: white;
+    border-radius: ${theme.materialDesign.borderRadius.default};
+    box-shadow: ${theme.shadows.default};
+`
