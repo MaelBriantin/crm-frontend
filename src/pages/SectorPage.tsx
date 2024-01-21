64653e936c793a97867219b2e3576e86423b908c
Maël Briantin <mael.briantin@proton.me>
2024-01-21 16:01:50 +0100

    feat(sectors): add useSectors custom hook and refactor SectorPage component

--- src/pages/SectorPage.tsx
+++ src/pages/SectorPage.tsx
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { isEmpty } from '../utils/helpers/spells.ts';
 import { Loader } from '../components/global/Loader.tsx';
-import { fetchAPI, handleAPIResponse } from '../services/api/fetchApi.config.ts';
 import { DataTable } from '../components/DataTable';
-import { SectorType } from '../types/DataTypes.ts';
+import { useSectors } from '../hooks/useSectors.ts';
+import { RowDataType } from '../types/DataTableTypes.ts';
 
 export const SectorPage: React.FC = () => {
-    const [sectors, setSectors] = useState<SectorType[]>([]);
 
-    useEffect(() => {
-        fetchSectors();
-    }, []);
-
-    const fetchSectors = async (): Promise<SectorType[] | []> => {
-        try {
-            const response = await fetchAPI<null, SectorType[]>('/api/sectorsWithPostcodes');
-
-            handleAPIResponse<SectorType>(
-                response,
-                (sectors) => {
-                    setSectors(sectors as SectorType[]);
-                },
-                (error) => {
-                    console.error(error.message);
-                }
-            );
-            return [];
-        } catch (error) {
-            console.error(error);
-            return [];
-        }
-    }
+    const sectors = useSectors('withPostcodes');
 
     const columns = [
         {
@@ -62,20 +39,11 @@ export const SectorPage: React.FC = () => {
         },
     ];
 
-    const sectorsData = sectors.map((sector) => {
-        return {
-            ...sector,
-            postcodes: sector.postcodes.map((postcode) => postcode.postcode),
-        };
-    });
-
     return (
         <Container>
             {isEmpty(sectors) && <Loader />}
             {!isEmpty(sectors) &&
-
-                <DataTable data={sectorsData} columns={columns} selectable />
-
+                <DataTable data={sectors as RowDataType[]} columns={columns} selectable />
             }
         </Container>
     );
