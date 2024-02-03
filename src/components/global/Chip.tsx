27156fc4b2c8eb2e1456ed8b165c47c7eb6ca1bf
Maël Briantin <mael.briantin@proton.me>
2024-02-03 23:04:30 +0100

    feat(sectors): refactor Chip component to support highlighting

--- src/components/global/Chip.tsx
+++ src/components/global/Chip.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { theme } from '../../assets/themes';
 
 type ChipProps = {
     text: string;
     color?: { background: string | undefined; text: string | undefined; } | undefined;
+    highlight?: (string | number | undefined)[];
 };
 
-export const Chip: React.FC<ChipProps> = ({ text, color }) => {
+export const Chip: React.FC<ChipProps> = ({ text, color, highlight }) => {
+
+    const setHighlight = (text: string, highlight: (string | number | undefined)[] | undefined): boolean => {
+        if (highlight) {
+            for (let i = 0; i < highlight.length; i++) {
+                if (text.includes(String(highlight[i])) && highlight[i] !== '') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     return (
-        <ChipContainer $background={color?.background ?? ''}>
-            <ChipText $text={color?.text ?? ''}>{text}</ChipText>
+        <ChipContainer
+            $background={color?.background ?? ''}
+            $highlight={setHighlight(text, highlight)}
+        >
+            <ChipText $text={color?.text ?? ''} dangerouslySetInnerHTML={{ __html: text }} />
         </ChipContainer>
     );
 };
 
-const ChipContainer = styled.div<{$background: string}>`
+const ChipContainer = styled.div<{ $background: string, $highlight: boolean }>`
     display: inline-flex;
     width: fit-content;
     padding: 4px 8px;
     border-radius: 16px;
-    background-color: ${({$background}): string => $background ? $background : '#e0e0e0'};
+    background-color: ${({ $background }): string => $background ? $background : '#e0e0e0'};
     margin-right: 8px;
+    ${({ $highlight }) => $highlight && css`outline: ${theme.colors.primary} solid 1px;`};
 `;
 
-const ChipText = styled.span<{$text: string}>`
+const ChipText = styled.span<{ $text: string }>`
     font-size: 14px;
-    color: ${({$text}): string => $text ? $text : '#333333'};
+    color: ${({ $text }): string => $text ? $text : `${theme.colors.dark}`};
 `;
