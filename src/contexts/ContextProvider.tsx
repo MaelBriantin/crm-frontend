43e38df3ba2f4bcc8b30de746a58c13a8216764f
Maël Briantin <mael.briantin@proton.me>
2024-02-16 21:57:06 +0100

    feat(sectors): add SectorsContext and SectorsProvider components

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { AuthProvider, ToastProvider, ModalProvider } from ".";
+import { 
+    AuthProvider, 
+    ToastProvider, 
+    ModalProvider, 
+    SectorsProvider, 
+    AppLoadingProvider 
+} from ".";
 
 interface ContextProviderProps {
     children: React.ReactNode;
 }
 
-export const ContextProvider: React.FC<ContextProviderProps> = ({children}) => {
+export const ContextProvider: React.FC<ContextProviderProps> = ({ children }) => {
     return (
         <AuthProvider>
-            <ToastProvider>
-                <ModalProvider>
-                    {children}
-                </ModalProvider>
-            </ToastProvider>
+            <AppLoadingProvider>
+                <SectorsProvider>
+                    <ToastProvider>
+                        <ModalProvider>
+                            {children}
+                        </ModalProvider>
+                    </ToastProvider>
+                </SectorsProvider>
+            </AppLoadingProvider>
         </AuthProvider>
     );
 };
