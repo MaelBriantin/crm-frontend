629833b2d6e842b25316aa1c48c0ddebcb893022
Maël Briantin <mael.briantin@proton.me>
2024-02-18 20:50:14 +0100

    feat(brands): refactor BrandPage component and add DataTable

--- src/pages/BrandPage.tsx
+++ src/pages/BrandPage.tsx
@@ -1,15 +1,77 @@
-import { useEffect } from 'react';
-import { fetchAllBrands } from '../services/api/brands/fetchAllBrands';
+import { useEffect, useState } from 'react';
+import { useBrands } from '../contexts';
+import { isEmpty } from '../utils/helpers/spells';
+import { DataTable } from '../components/DataTable';
+import { Loader } from '../components/global/Loader';
+import { RowDataType } from '../types/DataTableTypes';
+import styled from 'styled-components';
+import { deepCopy } from '../utils/helpers/spells';
 
 export const BrandPage = () => {
-    
+
+    const [sort, setSort] = useState<string | null>(null);
+    const [sortDirection, setSortDirection] = useState<boolean>(true);
+
+    const { refreshBrands, brands } = useBrands();
+
     useEffect(() => {
-        fetchAllBrands();
+        isEmpty(brands) && refreshBrands();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const columns = [
+        {
+            text: 'Marque',
+            value: 'name',
+            sortable: true,
+            width: '1000px'
+        },
+        {
+            text: 'Contact',
+            value: 'contact_name',
+            sortable: true,
+            width: '25%'
+        },
+        {
+            text: 'Email',
+            value: 'contact_email',
+            sortable: true,
+            width: '25%'
+        },
+        {
+            text: 'Téléphone',
+            value: 'contact_phone',
+            sortable: false,
+            width: '25%'
+        }
+    ];
+
     return (
-        <div>
-            <h1>Brand Page</h1>
-        </div>
+        <Container>
+            {isEmpty(brands) &&
+                <Loader />}
+            {!isEmpty(brands) &&
+                <DataTable
+                    searchbar
+                    hoverable
+                    columns={columns}
+                    data={deepCopy(brands) as RowDataType[]}
+                    sort={sort}
+                    setSort={setSort}
+                    sortDirection={sortDirection}
+                    setSortDirection={setSortDirection}
+                />}
+        </Container>
     );
 }
+
+const Container = styled.div`
+    position: relative;
+    width: 90%;
+    height: 95%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 20px;
+`;
