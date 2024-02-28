530bbfe23fc46efafb8be37984c4a554bc99cea8
Maël Briantin <mael.briantin@proton.me>
2024-02-28 01:06:00 +0100

    feat(brands): refactor DeleteAlert component to handle close and confirm actions

--- src/components/forms/DeleteAlert.tsx
+++ src/components/forms/DeleteAlert.tsx
@@ -1,24 +1,48 @@
 import { Alert, Button } from "../global";
 import { theme } from "../../assets/themes";
+import { useDeleteAlert } from "../../contexts";
+import { useState } from "react";
 
 type DeleteAlertProps = {
     message: string;
     confirmAction: () => void;
     cancelAction: () => void;
-    close?: boolean;
 };
 
-export const DeleteAlert: React.FC<DeleteAlertProps> = ({ message, confirmAction, cancelAction, close }) => {
+export const DeleteAlert: React.FC<DeleteAlertProps> = ({ message, confirmAction, cancelAction }) => {
+
+    const [close, setClose] = useState(false);
+    const [confirmed, setConfirmed] = useState(false);
+
+    const { setIsOpenDeleteAlert } = useDeleteAlert();
+
+    const handleConfirm = () => {
+        setClose(true);
+        setConfirmed(true);
+        setTimeout(() => {
+            confirmAction();
+            setIsOpenDeleteAlert(false);
+        }, 150);
+    }
+
+    const handleClose = () => {
+        setClose(true);
+        setTimeout(() => {
+            cancelAction();
+            setIsOpenDeleteAlert(false);
+        }, 150);
+    }
+
     return (
-        <Alert 
+        <Alert
             close={close}
-            type={'error'} 
-            absolute 
+            type={'error'}
+            absolute
             position='bottom'
         >
             <span dangerouslySetInnerHTML={{ __html: message }} />
-            <Button value='Confirmer' onClick={confirmAction} variant='small' color={theme.colors.error} />
-            <Button value='Annuler' onClick={cancelAction} variant='small' color={theme.colors.error} />
+            <Button loading={confirmed} value='Confirmer' onClick={handleConfirm} variant='small' color={theme.colors.error} />
+            <Button value='Annuler' onClick={handleClose} variant='small' color={theme.colors.error} />
         </Alert>
     )
 };
