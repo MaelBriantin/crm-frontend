a090e44e65b6e3a7d8a2861e8ed42d9165149bc4
Maël Briantin <mael.briantin@proton.me>
2024-02-29 23:55:21 +0100

    feat(customers): add index for pages

---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+export { LoginPage } from './auth/LoginPage';
+
+export { LoadingPage } from './LoadingPage';
+export { HomePage } from './HomePage';
+export { SectorPage } from './SectorPage';
+export { BrandPage } from './BrandPage';
+export { CustomerPage } from './CustomerPage';
+
+export { GuestLayout } from '../layouts/GuestLayout';
+export { AuthenticatedLayout } from '../layouts/AuthenticatedLayout';
