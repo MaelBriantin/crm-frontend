3b296829fdb4337769414b2fe46a9a5d610ae12c
Maël Briantin <mael.briantin@proton.me>
2024-03-03 20:31:04 +0100

    feat(customers): add onChange event handler and additional props to Switch component

--- src/components/global/Switch.tsx
+++ src/components/global/Switch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import { theme } from '../../assets/themes';
 import { getVariantStyle } from '../../utils/switchUtils';
@@ -6,7 +6,10 @@ import { getVariantStyle } from '../../utils/switchUtils';
 type SwitchProps = {
     vertical?: boolean;
     variant?: 'regular' | 'small';
-    onChange: () => void;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    checked?: boolean;
+    label?: string;
+    labelPostSwitch?: boolean;
 };
 
 type VariantStyleType = {
@@ -19,26 +22,36 @@ type VariantStyleType = {
     translate: string;
 };
 
-export const Switch: React.FC<SwitchProps> = ({ vertical = false, variant = 'regular', onChange }) => {
-    const [isChecked, setIsChecked] = useState(false);
+export const Switch: React.FC<SwitchProps> = ({ vertical = false, variant = 'regular', onChange, checked, label, labelPostSwitch }) => {
+    const [isChecked, setIsChecked] = useState(checked || false);
 
-    const handleToggle = () => {
-        onChange();
+    const handleToggle = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e);
         setIsChecked(!isChecked);
     };
 
     const variantStyle = getVariantStyle(variant, theme.colors.primary);
 
     return (
-        <SwitchContainer $vertical={vertical} $isChecked={isChecked} $variantSyle={variantStyle}>
-            <SwitchInput type="checkbox" checked={isChecked} onChange={handleToggle} $variantStyle={variantStyle} />
-            <SwitchSlider $variantStyle={variantStyle} />
+        <SwitchContainer>
+            {(label && !labelPostSwitch) && <SwitchLabel $vertical={vertical} $isChecked={isChecked}>{label}</SwitchLabel>}
+            <SwitchStyle $vertical={vertical} $isChecked={isChecked} $variantSyle={variantStyle}>
+                <SwitchInput type="checkbox" checked={isChecked} onChange={(e) => handleToggle(e)} $variantStyle={variantStyle} />
+                <SwitchSlider $variantStyle={variantStyle} />
+            </SwitchStyle>
+            {(label && labelPostSwitch) && <SwitchLabel $vertical={vertical} $isChecked={isChecked}>{label}</SwitchLabel>}
         </SwitchContainer>
     );
 };
 
+const SwitchContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`;
 
-const SwitchContainer = styled.label<{$vertical: boolean, $isChecked: boolean, $variantSyle: VariantStyleType}>`
+const SwitchStyle = styled.label<{ $vertical: boolean, $isChecked: boolean, $variantSyle: VariantStyleType }>`
     margin: ${({ $vertical, $variantSyle }): string => $vertical ? `0 -${$variantSyle.margin}px` : '0'};
     transition: all 0.2s ease-in-out;
     display: inline-block;
@@ -49,6 +62,11 @@ const SwitchContainer = styled.label<{$vertical: boolean, $isChecked: boolean, $
     border-radius: 10px;
     cursor: pointer;
     ${({ $vertical }): string => $vertical ? 'transform: rotate(90deg);' : ''};
+`;
+
+const SwitchLabel = styled.div<{ $vertical: boolean, $isChecked: boolean }>`
+    user-select: none;
+    color: ${({ $isChecked }): string => $isChecked ? theme.colors.primary : theme.colors.greyDark};
 `;
 
 const SwitchSlider = styled.span<{ $variantStyle: VariantStyleType }>`
