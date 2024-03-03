89af4054a324373768447c283d3f7fcfa660750c
Maël Briantin <mael.briantin@proton.me>
2024-03-03 20:50:44 +0100

    feat(customers): refactor sectorDropdownOptionFormat function to handle null sector

--- src/utils/customerUtils.ts
+++ src/utils/customerUtils.ts
@@ -2,14 +2,22 @@ import { DropdownOptions } from '../components/global/Dropdown';
 import { SectorType } from '../types/SectorTypes';
 
 
+
 /**
- * Formats a sector object into a dropdown option.
+ * Formats a sector object into a dropdown option. If the sector is null, it will return 'Hors secteur'.
  * @param sector - The sector object to format.
  * @returns The formatted dropdown option.
  */
 export const sectorDropdownOptionFormat = (sector: SectorType): DropdownOptions => {
-    return {
-        value: String(sector.id),
-        label: String(sector.name)
+    if(!sector) {
+        return {
+            value: '0',
+            label: 'Hors secteur'
+        }
+    } else {
+        return {
+            value: String(sector.id),
+            label: String(sector.name)
+        }
     }
 };
