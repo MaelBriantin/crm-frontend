bb9474a5a66b05489fe916d6eafcafd8b6431c4e
Maël Briantin <mael.briantin@proton.me>
2024-04-07 23:15:21 +0200

    feat(products): add useStoreProducts store for managing products state

---
+++ src/stores/useStoreProducts.ts
@@ -0,0 +1,69 @@
+import {create} from "zustand";
+import {ProductType} from "../types/ProductType.ts";
+import {fetchAllProducts, fetchProductOptions} from "../services/api/products";
+import {ProductOptionsType} from "../types/ProductType";
+import { firstOf } from "../utils/helpers/spells.ts";
+
+type createSectorsStore = {
+    products: ProductType[];
+    productOptions: ProductOptionsType;
+    vatRates: {label: string, value: string}[];
+    productTypes: {label: string, value: string}[];
+    measurementUnits: {label: string, value: string}[];
+    loadingProducts: boolean;
+    loadingOptions: boolean;
+    setLoadingProducts: (loadingProducts: boolean) => void;
+    fetchProducts: () => Promise<void>;
+    fetchProductOptions: () => Promise<void>;
+};
+
+export const useStoreProducts = create<createSectorsStore>((set) => ({
+
+    products: [],
+    productOptions: {} as ProductOptionsType,
+    vatRates: [],
+    productTypes: [],
+    measurementUnits: [],
+    loadingProducts: false,
+    loadingOptions: false,
+    setLoadingProducts: (loadingProducts: boolean) => set({ loadingProducts }),
+    fetchProducts: async () => {
+        set({ loadingProducts: true });
+        const response = await fetchAllProducts();
+        set({ products: response as ProductType[] });
+        set({ loadingProducts: false });
+    },
+    fetchProductOptions: async () => {
+        set({ loadingOptions: true });
+        const response = await fetchProductOptions();
+        set({ productOptions: firstOf(response) as ProductOptionsType });
+        const transformedProductOptions = transformProductOptions(firstOf(response) as ProductOptionsType);
+        set({ vatRates: transformedProductOptions.vatRates });
+        set({ productTypes: transformedProductOptions.ProductTypes });
+        set({ measurementUnits: transformedProductOptions.measurementUnits });
+        set({ loadingOptions: false });
+    }
+}));
+
+const transformProductOptions = (productOptions: ProductOptionsType) => {
+    const vatRatesDropdown: { label: string; value: string; }[] = [];
+    Object.entries(productOptions.vat_rates).map(([key, value]) => {
+        vatRatesDropdown.push({label: key, value: String(value)});
+    });
+    
+    const productTypesDropdown: { label: string; value: string; }[] = [];
+    Object.entries(productOptions.product_types).map(([key, value]) => {
+        productTypesDropdown.push({label: key, value: String(value)});
+    });
+
+    const measurementUnitsDropdown: { label: string; value: string; }[] = [];
+    Object.entries(productOptions.measurement_units).map(([key, value]) => {
+        measurementUnitsDropdown.push({label: key, value: String(value)});
+    });
+
+    return {
+        vatRates: vatRatesDropdown,
+        ProductTypes: productTypesDropdown,
+        measurementUnits: measurementUnitsDropdown
+    };
+}
