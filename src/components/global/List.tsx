437769f1344dfb0e5106db08e9df65440fd72b42
Maël Briantin <mael.briantin@proton.me>
2024-04-25 17:22:29 +0200

    feat(new-orders): update List component to use gris instead of flexbox and use selected array in props with selectKey as index of selection

--- src/components/global/List.tsx
+++ src/components/global/List.tsx
@@ -7,28 +7,28 @@ type ListProps<T extends Record<string, unknown>> = {
   data: T[];
   attributeList: { label: string; key: keyof T }[];
   multiple?: boolean;
-  onSelect?: (selected: T) => void;
+  selected?: number[];
+  selectKey: string;
+  setSelected: React.Dispatch<React.SetStateAction<number[]>>;
 };
 
 export const List = <T extends Record<string, unknown>>({
   data,
   attributeList,
   multiple,
+  selected = [],
+  setSelected,
+  selectKey,
 }: ListProps<T>) => {
-  const [selected, setSelected] = React.useState<number[]>([]);
-
-  const handleSelection = (event: React.MouseEvent<HTMLDivElement>) => {
-    const index = Number(event.currentTarget.dataset.index);
+    const handleSelection = (index: number) => {
+        console.log("index", index);
     if (selected.includes(index)) {
-      const newSelected = selected.filter(
-        (selectedIndex) => selectedIndex !== index
-      );
-      setSelected(newSelected);
+      setSelected(selected.filter((selectedIndex) => selectedIndex !== index));
     } else {
       if (!multiple) {
         setSelected([index]);
       } else {
-        setSelected((prevSelected) => [...prevSelected, index]);
+        setSelected([...selected, index]);
       }
     }
   };
@@ -39,8 +39,8 @@ export const List = <T extends Record<string, unknown>>({
         <ListElement
           key={index}
           data-index={index}
-          onClick={handleSelection}
-          $selected={selected.includes(index) || false}
+          onClick={() => handleSelection(element[selectKey] as number)}
+          $selected={selected.includes(element[selectKey] as number) || false}
         >
           <AttributeList>
             {attributeList.map((attribute, index) => (
@@ -49,7 +49,7 @@ export const List = <T extends Record<string, unknown>>({
               </React.Fragment>
             ))}
           </AttributeList>
-          {selected.includes(index) && (
+          {selected.includes(element[selectKey] as number) && (
             <SelectedAttribute>
               <VscPass />
             </SelectedAttribute>
@@ -71,14 +71,14 @@ const Container = styled.div`
 
 const ListElement = styled.div<{ $selected: boolean }>`
   height: ${theme.materialDesign.height.medium};
-  width: calc(95% - 40px);
+  width: calc(100% - 40px);
   border-top: 2px solid ${theme.colors.greyUltraLight};
   border-radius: ${theme.materialDesign.borderRadius.default};
   transition: all 0.25s;
   opacity: ${({ $selected }): number => ($selected ? 1 : 0.8)};
-  display: flex;
+  display: grid;
+  grid-template-columns: 8fr 2fr;
   align-items: center;
-  justify-content: space-between;
   gap: 5px;
   padding: 0 20px;
   background: ${({ $selected }): string =>
@@ -95,29 +95,28 @@ const ListElement = styled.div<{ $selected: boolean }>`
 const Attribute = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   font-size: ${theme.fonts.size.P1};
 `;
 
 const SelectedAttribute = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
   font-size: ${theme.fonts.size.P1};
   color: ${theme.colors.primary};
 `;
 
 const AttributeList = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   align-items: center;
-  justify-content: flex-start;
-  gap: 100px;
   width: 100%;
 `;
 
 const EmptyListElement = styled.div`
   height: ${theme.materialDesign.height.medium};
-  width: calc(95% - 40px);
+  width: calc(100% - 40px);
   border-top: 2px solid ${theme.colors.greyUltraLight};
   border-radius: ${theme.materialDesign.borderRadius.default};
   display: flex;
