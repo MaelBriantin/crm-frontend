ce8afb9aa741c75eedb30eb2e87a55362b122304
Maël Briantin <mael.briantin@proton.me>
2024-04-25 12:54:36 +0200

    feat(new-orders): add subtitle props to modal and ModalContext

--- src/contexts/global/ModalContext.tsx
+++ src/contexts/global/ModalContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
-import { Modal } from '../../components/Modal';
+import React, { createContext, useContext, useState, ReactNode } from "react";
+import { Modal } from "../../components/Modal";
 
 type ModalContextType = {
   isOpen: boolean;
@@ -9,38 +9,57 @@ type ModalContextType = {
   setStartCloseAnimation: (value: boolean) => void;
   disableClose: boolean;
   setDisableClose: (value: boolean) => void;
-}
+  title: string;
+  setTitle: (value: string) => void;
+  subTitle: string;
+  setSubTitle: (value: string) => void;
+};
 
 export type ModalProviderProps = {
   children: React.ReactNode;
-}
+};
 
 const ModalContext = createContext<ModalContextType>({
   isOpen: false,
-  showModal: () => { },
-  closeModal: () => { },
+  showModal: () => {},
+  closeModal: () => {},
   startCloseAnimation: false,
-  setStartCloseAnimation: () => { },
+  setStartCloseAnimation: () => {},
   disableClose: false,
-  setDisableClose: () => { }
+  setDisableClose: () => {},
+  title: "",
+  setTitle: () => {},
+  subTitle: "",
+  setSubTitle: () => {},
 });
 
-export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [disableClose, setDisableClose] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [content, setContent] = useState<ReactNode>(null);
   const [actions, setActions] = useState<ReactNode>(null);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
   const [startCloseAnimation, setStartCloseAnimation] = useState(false);
 
   const closeModal = () => {
     setStartCloseAnimation(true);
     setTimeout(() => {
       setIsOpen(false);
+      setContent(null);
+      setActions(null);
+      setTitle("");
+      setSubTitle("");
     }, 225);
   };
 
-  const showModal = (title: string, content: ReactNode, actions?: ReactNode) => {
+  const showModal = (
+    title: string,
+    content: ReactNode,
+    actions?: ReactNode
+  ) => {
     setContent(content);
     setActions(actions || null);
     setTitle(title);
@@ -48,10 +67,32 @@ export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   };
 
   return (
-    <ModalContext.Provider value={{ isOpen, closeModal, showModal, startCloseAnimation, setStartCloseAnimation, disableClose, setDisableClose }}>
+    <ModalContext.Provider
+      value={{
+        isOpen,
+        closeModal,
+        showModal,
+        startCloseAnimation,
+        setStartCloseAnimation,
+        disableClose,
+        setDisableClose,
+        title,
+        setTitle,
+        subTitle,
+        setSubTitle,
+      }}
+    >
       {children}
-      {isOpen &&
-        <Modal title={title} onClose={closeModal} children={content} disableClose={disableClose} actions={actions} />}
+      {isOpen && (
+        <Modal
+          title={title}
+          subTitle={subTitle}
+          onClose={closeModal}
+          children={content}
+          disableClose={disableClose}
+          actions={actions}
+        />
+      )}
     </ModalContext.Provider>
   );
 };
