e42c282923cbc2b7d47806b12f06afbf63a18614
Maël Briantin <mael.briantin@proton.me>
2024-04-27 14:03:17 +0200

    fix: fix NewOrderActionsContextProvider position in global ContextProvider

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,34 +1,34 @@
 import React from "react";
 import {
-    AuthProvider,
-    ToastProvider,
-    ModalProvider,
-    AppLoadingProvider,
-    DeleteAlertProvider,
-    FormActionsProvider,
-    NewOrderActionsProvider
+  AuthProvider,
+  ToastProvider,
+  ModalProvider,
+  AppLoadingProvider,
+  DeleteAlertProvider,
+  FormActionsProvider,
+  NewOrderActionsProvider,
 } from ".";
 
 interface ContextProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export const ContextProvider: React.FC<ContextProviderProps> = ({ children }) => {
-    return (
-        <AuthProvider>
-            <AppLoadingProvider>
-                <ToastProvider>
-                    <DeleteAlertProvider>
-                        <FormActionsProvider>
-                            <NewOrderActionsProvider>
-                            <ModalProvider>
-                                {children}
-                            </ModalProvider>
-                            </NewOrderActionsProvider>
-                        </FormActionsProvider>
-                    </DeleteAlertProvider>
-                </ToastProvider>
-            </AppLoadingProvider>
-        </AuthProvider>
-    );
+export const ContextProvider: React.FC<ContextProviderProps> = ({
+  children,
+}) => {
+  return (
+    <AuthProvider>
+      <AppLoadingProvider>
+        <ToastProvider>
+          <NewOrderActionsProvider>
+            <DeleteAlertProvider>
+              <FormActionsProvider>
+                <ModalProvider>{children}</ModalProvider>
+              </FormActionsProvider>
+            </DeleteAlertProvider>
+          </NewOrderActionsProvider>
+        </ToastProvider>
+      </AppLoadingProvider>
+    </AuthProvider>
+  );
 };
